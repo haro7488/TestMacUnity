05a1f58b07b0b99884f14ff27dfc812625875ff7
HARO_HOME_PC <haro7488@gmail.com>
2017-06-02 13:07:50 +0900

    움직임 커밋

---
+++ Assets/Scripts/MoveCtrl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCtrl : MonoBehaviour {
+
+    new Rigidbody rigidbody;
+    public float speed = 2f;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0f, v);
+        rigidbody.MovePosition(transform.position + (dir * Time.deltaTime * speed));
+
+    }
+}
